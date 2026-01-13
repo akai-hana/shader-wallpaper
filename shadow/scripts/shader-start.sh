217a84b87bbe366b59ea0d95b0cdd1b1d1046370
akai-hana <akaihanatsundeee@proton.me>
2026-01-13 21:03:36 +0100

    automated sync

--- shadow/scripts/shader-start.sh
+++ shadow/scripts/shader-start.sh
@@ -5,4 +5,4 @@ cd ~/git/shadow || exit 1
 SHADER=$(find ~/eudaimonia/shader-wallpaper/shadow -name "*.frag" | sort -R | head -n 1)
 
 # Run the shadow command in the background
-DRI_PRIME=1 poetry run shadow "$SHADER" -m root -f 24 -q 1 &
+DRI_PRIME=1 poetry run shadow "$SHADER" -m root -f 20 -q 1 &
